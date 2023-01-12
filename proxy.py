0233eb24a75e2ac5748c55ffdaa00ef1d96dd75a
OliLeiriao <oliver@leiriao.net>
2023-01-12 18:20:09 -0500

    finished exchange loop

--- proxy.py
+++ proxy.py
@@ -1,5 +1,6 @@
 import socket
 import threading
+import select
 
 SOCKS_VERSION = 1
 
@@ -40,6 +41,74 @@ class Proxy:
             domain_length = connection.recv(1)[0]
             address = connection.recv(domain_length)
             address = socket.gethostbyname(address)
+
+        # Convert bytes to unsigned short array
+        port = int.from_bytes(connection.recv(2), 'big', signed=False)
+
+        try:
+            if cmd == 1: # Connect
+                remote = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                remote.connect((address, port))
+                bind_address = remote.getsockname()
+                print("* Connected to {}:{}".format(address, port))
+            else:
+                connection.close()
+
+            addr = int.from_bytes(socket.inet_aton(bind_address[0]), 'big', signed=False)
+            port = bind_address[1]
+
+            # Connection reply for client
+            reply = b''.join([
+                SOCKS_VERSION.to_bytes(1, 'big'),
+                int(0).to_bytes(1, 'big'),
+                int(0).to_bytes(1, 'big'),
+                int(1).to_bytes(1, 'big'),
+                addr.to_bytes(4, 'big'),
+                port.to_bytes(2, 'big')
+            ])
+        
+        except Exception as e:
+            # Connection refused error in case of rejection
+            reply = self.generate_failed_reply(address_type, 5)
+
+        connection.sendall(reply)
+
+        # Establish data exchange
+        if reply[1] == 0 and cmd == 1:
+            # Loops while connected
+            self.exchange_loop(connection, remote)
+
+        # Finally, closes connection
+        connection.close()
+
+    
+    def exchange_loop(self, client, remote):
+        # Wait until client or remote is available for read
+        while True:
+            r, w, e = select.select([client, remote], )
+
+            if client in r:
+                # If the client's socket is readable, then read 4 KB and send it to the remote
+                data = client.recv(4096)
+                if remote.send(data) <= 0:
+                    break
+
+            if remote in r:
+                data = remote.recv(4096)
+                if client.send(data) <= 0:
+                    break
+
+
+
+    def generate_failed_reply(self, address_type, error_number):
+        return b''.join([
+            SOCKS_VERSION.to_bytes(1, 'big'),
+            error_number.to_bytes(1, 'big'),
+            int(0).to_bytes(1, 'big'),
+            address_type.to_bytes(1, 'big'),
+            int(0).to_bytes(4, 'big'),
+            int(0).to_bytes(4, 'big')
+        ])
 
     def verify_credentials(self, connection):
         version = ord(connection.recv(1)) # should be 1
