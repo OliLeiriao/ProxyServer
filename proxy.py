7588fc32720d85eb1ea64c48eb210ea3eefa411f
OliLeiriao <oliver@leiriao.net>
2023-01-12 18:00:58 -0500

    continued work on accepting connection

--- proxy.py
+++ proxy.py
@@ -18,6 +18,57 @@ class Proxy:
         # Get available methods [0, 1, 2]
         methods = self.get_available_methods(nmethods, connection)
 
+        # Accept only Username/Password authentication
+        if 2 not in set(methods):
+            # Close connection
+            connection.close()
+            return
+
+        # Welcome Message
+        connection.sendall(bytes([SOCKS_VERSION, 2]))
+
+        # Verify validity of credentials, if invalid do nothing.
+        if not self.verify_credentials(connection):
+            return
+
+        # Request version == 1
+        version, cmd, _, address_type = connection.recv(4)
+
+        if address_type == 1: # If IPv4 address
+            address = socket.inet_ntoa(connection.recv(4))
+        elif address_type == 3: # If Domain name
+            domain_length = connection.recv(1)[0]
+            address = connection.recv(domain_length)
+            address = socket.gethostbyname(address)
+
+    def verify_credentials(self, connection):
+        version = ord(connection.recv(1)) # should be 1
+
+        username_len = ord(connection.recv(1))
+        username = connection.recv(username_len).decode('utf-8')
+
+        password_len = ord(connection.recv(1))
+        password = connection.recv(password_len).decode('utf-8')
+
+        # Validate credentials
+        if username == self.username and password == self.password:
+            # Success status == 0
+            response = bytes([version, 0])
+            connection.sendall(response)
+            return True
+
+        # Else, Failed status != 1
+        response = bytes([version, 0xFF])
+        connection.sendall(response)
+        connection.close()
+        return False
+
+    def get_available_methods(self, nmethods, connection):
+        methods = []
+        for i in range(nmethods):
+            methods.append(ord(connection.recv(1)))
+        return methods
+    
     def run(self, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.bind(host,port)
